85343ade866c69a7bb25b42689c0dde9a91d5657
Ricky <ricky@rbwebdesigns.co.uk>
2021-08-21 23:43:39 +0100

    last_login field fix

--- app/modules/core/UserAccounts/Module.php
+++ app/modules/core/UserAccounts/Module.php
@@ -27,7 +27,7 @@ class Module
             `description` text,
             `admin` tinyint(1) NOT NULL DEFAULT '0',
             `signup_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `last_login` timestamp,
+            `last_login` datetime,
             `security_q` varchar(300),
             `security_a` varchar(55)
           ) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
