7a96ad60d593126ed843470802affa2f9effcee3
Julie Raymond <raymond.julie86@gmail.com>
2026-07-18 15:22:24 +0200

    refactor: clean code improvement (Magic Numbers)

--- modules/UserAccounts/Module.php
+++ modules/UserAccounts/Module.php
@@ -1,4 +1,4 @@
-<?php
+const SECURITY_ANSWER_LENGTH = 55;const SECURITY_QUESTION_LENGTH = 300;const RESET_TOKEN_LENGTH = 50;const GENDER_LENGTH = 10;const PROFILE_PICTURE_LENGTH = 255;const LOCATION_LENGTH = 50;const PASSWORD_LENGTH = 255;const USERNAME_LENGTH = 30;const ID_LENGTH = 8;<?php
 
 namespace HamletCMS\UserAccounts;
 
@@ -14,23 +14,23 @@ class Module
         $dbc = HamletCMS::databaseConnection();
 
         $dbc->query("CREATE TABLE `users` (
-            `id` int(8) NOT NULL,
-            `name` varchar(30) NOT NULL,
+            `id` self::ID_LENGTH NOT NULL,
+            `name` self::USERNAME_LENGTH NOT NULL,
             `surname` varchar(30) NOT NULL,
             `username` varchar(30) NOT NULL,
-            `password` varchar(255) NOT NULL,
-            `email` varchar(255) NOT NULL,
+            `password` self::PASSWORD_LENGTH NOT NULL,
+            `email` self::PROFILE_PICTURE_LENGTH NOT NULL,
             `dob` date,
-            `gender` varchar(10),
-            `location` varchar(50),
+            `gender` self::GENDER_LENGTH,
+            `location` self::LOCATION_LENGTH,
             `profile_picture` varchar(255),
             `description` text,
             `admin` tinyint(1) NOT NULL DEFAULT '0',
             `signup_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
             `last_login` datetime,
-            `reset_token` varchar(50),
-            `security_q` varchar(300),
-            `security_a` varchar(55)
+            `reset_token` self::RESET_TOKEN_LENGTH,
+            `security_q` self::SECURITY_QUESTION_LENGTH,
+            `security_a` self::SECURITY_ANSWER_LENGTH
           ) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
 
         $dbc->query("ALTER TABLE `users` ADD PRIMARY KEY (`id`);");
